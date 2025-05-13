dda4461fcfb45c3973f19fcf0c7c8a9a676cb50b
Mostafa Deghady <mdeghady>
2025-05-13 14:34:41 +0200

    add cleaning part to spider

--- unifiedscraper/spiders/goccia-men.py
+++ unifiedscraper/spiders/goccia-men.py
@@ -15,13 +15,19 @@ class GocciaMen(NextPageScraper, DataCleanser):
         "Brand": response.css(product_schema['Brand']).get(),
         "ProductName": response.css(product_schema['ProductName']).get(),
         "ProductImage": response.css(product_schema['ProductImage']).get(),
-        "CurrentPrice": response.css(product_schema['CurrentPrice']).getall()[0],
-        "OriginalPrice": response.css(product_schema['OldPrice']).getall()[0],
         "AvailableSizes": response.css(product_schema['AvailableSizes']).getall(),
         "Category": response.css(product_schema['Category']).get(),
         "Collection": response.css(product_schema['Collection']).get(),
         "sku": response.css(product_schema['sku']).get(),
         }
         # Clean the product data
+        no_discount_price = response.css(product_schema['NoDiscountPrice']).get()
+        if no_discount_price:
+            product['CurrentPrice'] = no_discount_price
+            product['OriginalPrice'] = no_discount_price
+        else:
+            product['CurrentPrice'] = response.css(product_schema['CurrentPrice']).get()
+            product['OriginalPrice'] = response.css(product_schema['OldPrice']).get()
+        product["PriceCurrency"] = self._convert_currency_symbols_to_code(product['CurrentPrice'])
 
         yield product
