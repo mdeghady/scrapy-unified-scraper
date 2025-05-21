c146f6e2ea9f2eea0befc8ea0af03ec350d3edfb
Mostafa Deghady <mdeghady>
2025-05-21 10:40:31 +0200

    Added evolution website to be scraped

---
+++ unifiedscraper/spiders/evolution.py
@@ -0,0 +1,54 @@
+import json
+import re
+
+from unifiedscraper.spiders.base_scraper import NextPageScraper, DataCleanser
+
+
+class Evolution(NextPageScraper , DataCleanser):
+    """Spider for Deflorio store"""
+
+    name = "evolution"
+    allowed_domains = ["www.evolutionsessa.com"]
+    start_urls = ["https://www.evolutionsessa.com"]
+
+    def parse_product_page(self, response):
+        """Parse product page and extract product data"""
+        product_schema = self.schema['product_page_schema']
+        product = {
+            "Brand": response.css(product_schema['Brand']).get(),
+            "ProductName": response.css(product_schema['ProductName']).get(),
+            "ProductImage": response.css(product_schema['ProductImage']).get(),
+            "ProductDetails": response.css(product_schema['ProductDetails']).get(),
+            "CurrentPrice": response.css(product_schema['CurrentPrice']).get(),
+            "OriginalPrice": response.css(product_schema['OldPrice']).get(),
+            "AvailableSizes": response.css(product_schema['AvailableSizes']).getall(),
+            "Category": response.css(product_schema['Category']).get(),
+            "sku": response.css(product_schema['sku']).get(),
+            "ProductURL": response.url,
+        }
+
+        # Clean the data
+        #clean price data
+        product['PriceCurrency'] = self._convert_currency_symbols_to_code(product['CurrentPrice'])
+        product['CurrentPrice'] = float(
+            re.search(r'[\d.]+', product['CurrentPrice']).group())
+        if product['OriginalPrice']:
+            product['OriginalPrice'] = float(
+                re.search(r'[\d.]+', product['OriginalPrice']).group())
+        else:
+            product['OriginalPrice'] = product['CurrentPrice']
+
+
+        #clean sku data
+        sku_splitted = product['sku'].replace("SKU:" , "").split(" ")[0].split('-')[0].split('_')[0]
+        product['ProductCode'] = sku_splitted
+
+        try:
+            product_details = json.loads(product.pop('ProductDetails'))
+            product['ProductColor'] = product_details.get('color')
+            product['Availability'] = product_details['offers']['offers'][0]['availability']
+        except:
+            product['ProductColor'] = None
+            product['Availability'] = None
+
+        yield product
